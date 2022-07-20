cb81313b2f4e031e8dbc12f86ef443d47195ae97
藤井崇仁 <ftk1031rs@outlook.jp>
2022-07-21 07:40:43 +0900

    2022/07/21_ディレクトリ構成の変更

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import java.util.List;
 
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.PokeData;
+import com.example.demo.repository.PokeDataRepository;
 
 @Controller
 public class MainController {
@@ -15,8 +18,8 @@ public class MainController {
      */
     @Autowired
     private PokeDataRepository repository;
-	
-	
+
+
 	@GetMapping("/test1")
 	public String write1(Model model) {
 		model.addAttribute("moji", "hello world!");
--- src/main/java/com/example/demo/repository/PokeDataRepository.java
+++ src/main/java/com/example/demo/repository/PokeDataRepository.java
@@ -1,10 +1,12 @@
-package com.example.demo;
+package com.example.demo.repository;
 
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.PokeData;
 
 @Repository
 public interface PokeDataRepository extends JpaRepository<PokeData, Long>{
